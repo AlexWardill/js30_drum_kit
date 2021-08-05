e3c4737ae68ec00038d1a2b38138b4b45d919c83
Alex Wardill <alexander@wardill.net>
2021-08-05 23:49:29 +0100

    initiliased consts, add audio

---
+++ app.js
@@ -0,0 +1,13 @@
+const header = document.getElementById('h1');
+const container = document.getElementById('container');
+const boxes = document.querySelectorAll('.box');
+const kick_A = document.getElementById('kick');
+const snare_S = document.getElementById('snare');
+const hat_D = document.getElementById('hat');
+const crash_F = document.getElementById('crash');
+const ride_G = document.getElementById('ride');
+const htom_H = document.getElementById('htom');
+const ltom_J = document.getElementById('ltom');
+const ftom_K = document.getElementById('ftom');
+const cowbell = document.getElementById('cowbell');
+
